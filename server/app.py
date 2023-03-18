acca6f0f06503b6015113c5c726731013d9d7c2d
sbalayan1 <balayans2014@yahoo.com>
2023-03-17 22:46:49 -0700

    complete

--- server/app.py
+++ server/app.py
@@ -19,15 +19,38 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if animal:
+        # attributes = ['name', 'species', 'zookeeper']
+        # listElems = [f'<li>{animal.attr}</li>' for attr in Animal.keys()]
+        response = f'''
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper.name}</ul>
+            <ul>Enclosure: {animal.enclosure.environment}</ul>
+        '''
+        return make_response(response, 200)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    res = ""
+    res += f"<ul>Name: {zookeeper.name}</ul>"
+    res += f"<ul>Birthday: {zookeeper.birthday}</ul>"
+    res += f"<ul>Animals: {zookeeper.animals}</ul>"
+
+
+    return make_response(res, 200)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enc = Enclosure.query.filter(Enclosure.id == id).first()
+    res = ""
+    res += f"<ul>Environment: {enc.environment}</ul>"
+    res += f"<ul>Open to Visitors: {enc.open_to_visitors}</ul>"
+    res += f"<ul>Animals: {enc.animals}</ul>"
+
+    return make_response(res, 200)
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -48,3 +48,4 @@ with app.app_context():
     db.session.add_all(animals)
     db.session.commit()
 
+print('seeded')
